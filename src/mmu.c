77c8fffacce216477d59fb3fe785b8b4f0ed4d0f
Nicolás Berríos Verboven <nbverboven@gmail.com>
2017-06-21 16:28:37 -0300

    Agregué funciones para mapear y desmapear páginas

--- src/mmu.c
+++ src/mmu.c
@@ -9,24 +9,24 @@
 
 unsigned int proxima_pagina_libre;
 
-void mmu_inicializar() {	
-  proxima_pagina_libre = 0x29000;
+void mmu_inicializar() {
+	proxima_pagina_libre = 0x29000;
 }
 
 void mmu_inicializar_dir_kernel() {
 
-  // Inicializo directorio de paginas en pos 0x27000
+	// Inicializo directorio de paginas en pos 0x27000
 	int* page_directory = (int*) 0x27000;  // PAGE_DIRECTORY_KERNEL = 0x27000
-  // Seteo directorio de tablas en pos 0x28000
-  page_directory[0] = 0x28000 + 0x3;		 // PAGE_TABLE_KERNEL = 0x28000, seteo p=1 y r/w=1
+	// Seteo directorio de tablas en pos 0x28000
+	page_directory[0] = 0x28000 + 0x3;		 // PAGE_TABLE_KERNEL = 0x28000, seteo p=1 y r/w=1
 
-  // Pongo todo el resto de las posiciones en cero.
+	// Pongo todo el resto de las posiciones en cero.
 	for (int i = 1; i < 1024; ++i) { 
-    page_directory[i]= 0x0;
+	page_directory[i]= 0x0;
 	}
 
-  // Inicializo page_directory 
-  int* page_table_1 = (int*) 0x28000;
+	// Inicializo page_directory 
+	int* page_table_1 = (int*) 0x28000;
 	for (int i = 0; i < 1024; ++i) {
 		page_table_1[i] = ((i << 12) | 3);
 	}
@@ -42,27 +42,92 @@ unsigned int mmu_proxima_pagina_fisica_libre(){
 
 
 int* mmu_inicializar_dir_zombi(unsigned int id_tarea, unsigned int codigo_tarea){//TODO ver parametros necesarios
-    int* page_directory = (int*) mmu_proxima_pagina_fisica_libre();  
-    // Inicializo en 0x0
-    for (int i = 1; i < 1024; ++i) {
-      page_directory[i]= 0x0;
-    }
+	int* page_directory = (int*) mmu_proxima_pagina_fisica_libre();  
+	// Inicializo en 0x0
+	for (int i = 1; i < 1024; ++i) {
+	  page_directory[i] = 0x0;
+	}
 
-    int* page_table_1 = (int*) mmu_proxima_pagina_fisica_libre();
-    for (int i = 0; i < 1024; ++i) {
-      page_table_1[i] = ((i << 12) | 7); //user,read only,present
-    }
+	int* page_table_1 = (int*) mmu_proxima_pagina_fisica_libre();
+	for (int i = 0; i < 1024; ++i) {
+		page_table_1[i] = ((i << 12) | 7); //user,read only,present
+	}
 
-    // Seteo la pagina_1 en el page directory del dir_zombi
-	  page_directory[0] = (int)page_table_1 + 0x7;
+	// Seteo la pagina_1 en el page directory del dir_zombi
+	page_directory[0] = (int)page_table_1 + 0x7;
 
 
 
-    //Setear mapa en el page directory
+	//Setear mapa en el page directory
 
-    return page_directory;
+	return page_directory;
 
 
 }
 
 
+void mmu_mapear_pagina(unsigned int virtual, unsigned int dir_pd, unsigned int fisica, 
+	                   unsigned char rw, unsigned char us)
+{
+	unsigned int indice_pd = (virtual & 0xFFC00000) >> 22; // Me quedo con los bits 22-31
+	unsigned int indice_pt = (virtual & 0x003FF000) >> 12; // Me quedo con los bits 12-21
+
+	pd_entry* page_directory = (pd_entry*) dir_pd;
+
+	// Si la tabla de páginas no existe, la creo
+	if ( !(page_directory[indice_pd].present) )
+	{
+		int* nueva_pt = (int*) mmu_proxima_pagina_fisica_libre();
+
+		for (int i = 0; i < 1024; ++i)
+		{
+			nueva_pt[i] = 0;
+		}
+
+		page_directory[indice_pd].present = 1;
+		page_directory[indice_pd].read_write = rw;
+		page_directory[indice_pd].user_supervisor = us;
+		page_directory[indice_pd].page_table_address = (unsigned int) nueva_pt >> 12;
+	}
+
+	// Completo la entrada correspondiente de la tabla de páginas
+	pt_entry* page_table = (pt_entry*) ( page_directory[indice_pd].page_table_address << 12 );
+	page_table[indice_pt].present = 1;
+	page_table[indice_pt].read_write = rw;
+	page_table[indice_pt].user_supervisor = us;
+	page_table[indice_pt].page_frame_address = fisica >> 12;
+
+	tlbflush();
+}
+
+
+void mmu_desmapear_pagina(unsigned int virtual, unsigned int dir_pd)
+{
+	unsigned int indice_pd = (virtual & 0xFFC00000) >> 22; // Me quedo con los bits 22-31
+	unsigned int indice_pt = (virtual & 0x003FF000) >> 12; // Me quedo con los bits 12-21
+
+	pd_entry* page_directory = (pd_entry*) dir_pd;
+
+	if ( page_directory[indice_pd].present )
+	{
+		pt_entry* page_table = (pt_entry*) ( page_directory[indice_pd].page_table_address << 12 );
+		page_table[indice_pt].present = 0;
+
+		int nunguna_pagina_presente = 1;
+
+		for ( int i = 0; i < 1024; i++)
+		{
+			if ( page_table[i].present == 1 )
+			{
+				nunguna_pagina_presente = 0;
+			}
+		}
+
+		if ( nunguna_pagina_presente )
+		{
+			page_directory[indice_pd].present = 0;
+		}
+	}
+
+	tlbflush();
+}
--- src/mmu.h
+++ src/mmu.h
@@ -16,6 +16,49 @@
 void mmu_inicializar();
 void mmu_inicializar_dir_kernel();
 void mmu_proxima_pagina_libre();
+void mmu_mapear_pagina(unsigned int virtual, unsigned int dir_pd, unsigned int fisica, unsigned char rw, unsigned char us);
+void mmu_desmapear_pagina(unsigned int virtual, unsigned int dir_pd);
+
+
+typedef struct str_pd_entry {
+	unsigned char present:1;
+	unsigned char read_write:1;                 // 1 = readable/writable; 0 = read only
+	unsigned char user_supervisor:1;            // 1 = user; 0 = supervisor
+	unsigned char page_level_write_through:1;
+	unsigned char page_level_cache_disabled:1;
+	unsigned char accesed:1;
+	unsigned char ignored_1:1;
+	unsigned char page_size:1;                  // 0 = 4Kb, 1 = 4Mb
+	unsigned char ignored_2:4;
+	unsigned int page_table_address:20;         // Dirección física de la tabla de página
+} __attribute__((__packed__)) pd_entry;
+
+
+typedef struct str_pt_entry {
+	unsigned char present:1;
+	unsigned char read_write:1;                 // 1 = readable/writable; 0 = read only
+	unsigned char user_supervisor:1;            // 1 = user; 0 = supervisor
+	unsigned char page_level_write_through:1;
+	unsigned char page_level_cache_disabled:1;
+	unsigned char accesed:1;
+	unsigned char dirty:1;                      // Si la página fue modificada, se setea en 1
+	unsigned char page_attribute_table:1;       // (?)
+	unsigned char global:1;                     // (?)
+	unsigned char ignored:3;
+	unsigned int page_frame_address:20;         // Dirección física del frame de una página de 4Kb
+} __attribute__((__packed__)) pt_entry;
+
+
+typedef struct str_dir_virtual {
+	unsigned short offset:12;
+	unsigned short page_table_index:10;
+	unsigned short page_directory_index:10;
+} __attribute__((__packed__)) dir_virtual;
+
+
+
+//extern pd_entry page_directory[];
+//extern pt_entry page_table[];
 
 #endif	/* !__MMU_H__ */
 
