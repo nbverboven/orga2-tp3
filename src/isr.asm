8cc06caf10af52b347280a859e8220195c534b80
Nicolás Berríos Verboven <nbverboven@gmail.com>
2017-07-17 20:16:40 -0300

    Imprimir pantalla debug

--- src/isr.asm
+++ src/isr.asm
@@ -14,30 +14,30 @@ BITS 32
 sched_tarea_offset:     dd 0x00
 sched_tarea_selector:   dw 0x00
 
-pintar_pantalla_msg db ''
+; pintar_pantalla_msg db ''
 
-msg_debug_hex db '0123456789ABCDEF'
-msg_debug_eax db 'eax'
-msg_debug_ebx db 'ebx'
-msg_debug_ecx db 'ecx'
-msg_debug_edx db 'edx'
-msg_debug_esi db 'esi'
-msg_debug_edi db 'edi'
-msg_debug_ebp db 'ebp'
-msg_debug_esp db 'esp'
-msg_debug_eip db 'eip'
-msg_debug_cs db 'cs'
-msg_debug_ds db 'ds'
-msg_debug_es db 'es'
-msg_debug_fs db 'fs'
-msg_debug_gs db 'gs'
-msg_debug_ss db 'ss'
-msg_debug_eflags db 'eflags'
-msg_debug_cr0 db 'cr0'
-msg_debug_cr2 db 'cr2'
-msg_debug_cr3 db 'cr3'
-msg_debug_cr4 db 'cr4'
-msg_debug_stack db 'stack'
+; msg_debug_hex db '0123456789ABCDEF'
+; msg_debug_eax db 'eax'
+; msg_debug_ebx db 'ebx'
+; msg_debug_ecx db 'ecx'
+; msg_debug_edx db 'edx'
+; msg_debug_esi db 'esi'
+; msg_debug_edi db 'edi'
+; msg_debug_ebp db 'ebp'
+; msg_debug_esp db 'esp'
+; msg_debug_eip db 'eip'
+; msg_debug_cs db 'cs'
+; msg_debug_ds db 'ds'
+; msg_debug_es db 'es'
+; msg_debug_fs db 'fs'
+; msg_debug_gs db 'gs'
+; msg_debug_ss db 'ss'
+; msg_debug_eflags db 'eflags'
+; msg_debug_cr0 db 'cr0'
+; msg_debug_cr2 db 'cr2'
+; msg_debug_cr3 db 'cr3'
+; msg_debug_cr4 db 'cr4'
+; msg_debug_stack db 'stack'
 
 msg_int_0 db 'Devide error wacho!'
 len_int_0 equ $ - msg_int_0
@@ -112,6 +112,9 @@ extern game_jugador_mover
 
 ;; Screen
 extern actualizar_info_pantalla
+extern restaurar_pantalla
+extern imprimir_pantalla_debug
+extern info_debug
 
 ;;
 ;; Definición de MACROS
@@ -120,10 +123,53 @@ extern actualizar_info_pantalla
 global _isr%1
 
 _isr%1:
-	pushad
+	; pushad
 
-	mov eax, %1
+	; mov eax, %1
 	imprimir_texto_mp  msg_int_%1, len_int_%1, 0x07, 0, 0
+
+	mov dword [info_debug + 0], eax
+	mov dword [info_debug + 4], ebx
+	mov dword [info_debug + 8], ecx
+	mov dword [info_debug + 12], edx
+	mov dword [info_debug + 16], esi
+	mov dword [info_debug + 20], edi
+	mov dword [info_debug + 24], ebp
+	mov dword [info_debug + 28], esp
+	mov eax, [esp + 4]
+	mov dword [info_debug + 32], eax
+	mov word [info_debug + 36], cs
+	mov word [info_debug + 38], ds
+	mov word [info_debug + 40], es
+	mov word [info_debug + 42], fs
+	mov word [info_debug + 44], gs
+	mov word [info_debug + 46], ss
+	pushfd
+	pop eax
+	mov dword [info_debug + 48], eax
+	mov eax, cr0
+	mov dword [info_debug + 52], eax
+	mov eax, cr2
+	mov dword [info_debug + 56], eax
+	mov eax, cr3
+	mov dword [info_debug + 60], eax
+	mov eax, cr4
+	mov dword [info_debug + 64], eax
+	mov eax, [esp + 4]
+	mov dword [info_debug + 68], eax
+	mov eax, [esp + 8]
+	mov dword [info_debug + 72], eax
+	mov eax, [esp + 12]
+	mov dword [info_debug + 76], eax
+	mov eax, [esp + 16]
+	mov dword [info_debug + 80], eax
+	mov eax, [esp + 20]
+	mov dword [info_debug + 84], eax
+
+	call imprimir_pantalla_debug
+
+; Esto está para ver como queda. Después se comenta o se saca
+jmp $
 
 	call sched_desalojar_tarea_actual
 
@@ -131,45 +177,46 @@ _isr%1:
 	mov [sched_tarea_selector], ax ; Cargo el selector de tss de la tarea idle
 	jmp far [sched_tarea_offset]
 
-	popad
+	; popad
 	iret
 
 %endmacro
 
 
-%macro imprimir_hexa 4
-	pushad
-	pushfd
+; %macro imprimir_hexa 4
+; 	pushad
+; 	pushfd
     
-    mov eax, %1 ; %1 = registro
-	mov esi, %2 ; %2 = longitud
-    mov ecx, %3	; %3 = fila
-	mov edx, %4	; %4 = columna
+;     mov eax, %1 ; %1 = registro
+; 	mov esi, %2 ; %2 = longitud
+;     mov ecx, %3	; %3 = fila
+; 	mov edx, %4	; %4 = columna
 	
-	add esi, edx
+; 	add esi, edx
 
-	%%impimo:
-		mov ebx, eax
-		mov bl, bl
-		add ebx, msg_debug_hex
-		imprimir_texto_mp ebx, 1, 0x7F, ecx, edx
-		inc edx
-		shr eax, 8
+; 	%%impimo:
+; 		mov ebx, eax
+; 		mov bl, bl
+; 		add ebx, msg_debug_hex
+; 		imprimir_texto_mp ebx, 1, 0x7F, ecx, edx
+; 		inc edx
+; 		shr eax, 8
 
-		cmp edx, esi
-		jne %%impimo
+; 		cmp edx, esi
+; 		jne %%impimo
 
-	popfd
-	popad
-%endmacro
+; 	popfd
+; 	popad
+; %endmacro
 
 ;;
 ;; Datos
 ;; -------------------------------------------------------------------------- ;;
 ; Scheduler
-isrnumero:           dd 0x00000000
-isrClock:            db '|/-\'
-estoy_en_modo_debug: db 0
+isrnumero:              dd 0x00000000
+isrClock:               db '|/-\'
+estoy_en_modo_debug:    db 0
+mostrando_pantalla_debug: db 0
 
 
 ;;
@@ -259,13 +306,13 @@ _isr102:
 	pushfd
 
 	push eax	
-	call is_mode_debug_on
-	cmp ax, 0
-	je .debug_off
-		call ventana_debug
-		jmp .fin
+	; call is_mode_debug_on
+	; cmp ax, 0
+	; je .debug_off
+	; 	call ventana_debug
+	; 	jmp .fin
 
-	.debug_off:
+	; .debug_off:
 		call game_move_current_zombi
 		;call sched_ejecutar_orden_66
 		add esp, 4
@@ -302,97 +349,103 @@ proximo_reloj:
 
 ;; Ventada Debug
 ;; -------------------------------------------------------------------------- ;;
-ventana_debug:
-	;50*36 
+; ventana_debug:
+; 	;50*36 
 
-	imprimir_texto_mp pintar_pantalla_msg, 30, 0x00, 7, 24
-	imprimir_texto_mp pintar_pantalla_msg, 1, 0x00, 8, 24
-	imprimir_texto_mp pintar_pantalla_msg, 28, 00010001b, 8, 25
-	imprimir_texto_mp pintar_pantalla_msg, 1, 0x00, 8, 53
+; 	imprimir_texto_mp pintar_pantalla_msg, 30, 0x00, 7, 24
+; 	imprimir_texto_mp pintar_pantalla_msg, 1, 0x00, 8, 24
+; 	imprimir_texto_mp pintar_pantalla_msg, 28, 00010001b, 8, 25
+; 	imprimir_texto_mp pintar_pantalla_msg, 1, 0x00, 8, 53
 
-	mov ecx, 34
-	mov ecx, ecx
+; 	mov ecx, 34
+; 	mov ecx, ecx
 
-	.background_ventana:
-		mov eax, ecx
-		add eax, 8
-		imprimir_texto_mp pintar_pantalla_msg, 30, 0x00, eax, 24
-		imprimir_texto_mp pintar_pantalla_msg, 28, 0x77, eax, 25
-	loop .background_ventana
+; 	.background_ventana:
+; 		mov eax, ecx
+; 		add eax, 8
+; 		imprimir_texto_mp pintar_pantalla_msg, 30, 0x00, eax, 24
+; 		imprimir_texto_mp pintar_pantalla_msg, 28, 0x77, eax, 25
+; 	loop .background_ventana
 
-	imprimir_texto_mp pintar_pantalla_msg, 30, 0x00, 42, 24
+; 	imprimir_texto_mp pintar_pantalla_msg, 30, 0x00, 42, 24
 
 
-	;; --- IMPRIMO LOS REGISTROS ---
-	;popfd
-	;popad
+; 	;; --- IMPRIMO LOS REGISTROS ---
+; 	;popfd
+; 	;popad
 
-	;xchg bx,bx ;breakpoint
+; 	;xchg bx,bx ;breakpoint
 
-	imprimir_texto_mp msg_debug_eax, 3, 0x70, 10, 26
-	imprimir_hexa eax, 8, 10, 30
+; 	imprimir_texto_mp msg_debug_eax, 3, 0x70, 10, 26
+; 	imprimir_hexa eax, 8, 10, 30
 
-	imprimir_texto_mp msg_debug_ebx, 3, 0x70, 12, 26
-	imprimir_hexa ebx, 8, 12, 30
+; 	imprimir_texto_mp msg_debug_ebx, 3, 0x70, 12, 26
+; 	imprimir_hexa ebx, 8, 12, 30
 
-	imprimir_texto_mp msg_debug_ecx, 3, 0x70, 14, 26
-	imprimir_hexa ecx, 8, 14, 30
+; 	imprimir_texto_mp msg_debug_ecx, 3, 0x70, 14, 26
+; 	imprimir_hexa ecx, 8, 14, 30
 
-	imprimir_texto_mp msg_debug_edx, 3, 0x70, 16, 26
-	imprimir_hexa edx, 8, 16, 30
+; 	imprimir_texto_mp msg_debug_edx, 3, 0x70, 16, 26
+; 	imprimir_hexa edx, 8, 16, 30
 
-	imprimir_texto_mp msg_debug_esi, 3, 0x70, 18, 26
-	imprimir_hexa esi, 8, 18, 30
+; 	imprimir_texto_mp msg_debug_esi, 3, 0x70, 18, 26
+; 	imprimir_hexa esi, 8, 18, 30
 
-	imprimir_texto_mp msg_debug_edi, 3, 0x70, 20, 26
-	imprimir_hexa edi, 8, 20, 30
+; 	imprimir_texto_mp msg_debug_edi, 3, 0x70, 20, 26
+; 	imprimir_hexa edi, 8, 20, 30
 
-	imprimir_texto_mp msg_debug_ebp, 3, 0x70, 22, 26
-	imprimir_hexa ebp, 8, 22, 30
+; 	imprimir_texto_mp msg_debug_ebp, 3, 0x70, 22, 26
+; 	imprimir_hexa ebp, 8, 22, 30
 
-	imprimir_texto_mp msg_debug_esp, 3, 0x70, 24, 26
-	imprimir_hexa esp, 8, 24, 30
+; 	imprimir_texto_mp msg_debug_esp, 3, 0x70, 24, 26
+; 	imprimir_hexa esp, 8, 24, 30
 
-	imprimir_texto_mp msg_debug_eip, 3, 0x70, 26, 26
-	imprimir_hexa [esp], 8, 26, 30
+; 	imprimir_texto_mp msg_debug_eip, 3, 0x70, 26, 26
+; 	imprimir_hexa [esp+4], 8, 26, 30
 	
-	imprimir_texto_mp msg_debug_cs, 2, 0x70, 28, 27
-	imprimir_hexa cs, 4, 28, 30
+; 	imprimir_texto_mp msg_debug_cs, 2, 0x70, 28, 27
+; 	imprimir_hexa cs, 4, 28, 30
 
-	imprimir_texto_mp msg_debug_ds, 2, 0x70, 30, 27
-	imprimir_hexa ds, 4, 32, 30
+; 	imprimir_texto_mp msg_debug_ds, 2, 0x70, 30, 27
+; 	imprimir_hexa ds, 4, 32, 30
 
-	imprimir_texto_mp msg_debug_es, 2, 0x70, 32, 27
-	imprimir_hexa es, 4, 32, 30
+; 	imprimir_texto_mp msg_debug_es, 2, 0x70, 32, 27
+; 	imprimir_hexa es, 4, 32, 30
 
-	imprimir_texto_mp msg_debug_fs, 2, 0x70, 34, 27
-	imprimir_hexa fs, 4, 34, 30
+; 	imprimir_texto_mp msg_debug_fs, 2, 0x70, 34, 27
+; 	imprimir_hexa fs, 4, 34, 30
 
-	imprimir_texto_mp msg_debug_gs, 2, 0x70, 36, 27
-	imprimir_hexa gs, 4, 36, 30
+; 	imprimir_texto_mp msg_debug_gs, 2, 0x70, 36, 27
+; 	imprimir_hexa gs, 4, 36, 30
 
-	imprimir_texto_mp msg_debug_ss, 2, 0x70, 38, 27
-	imprimir_hexa ss, 4, 38, 30
+; 	imprimir_texto_mp msg_debug_ss, 2, 0x70, 38, 27
+; 	imprimir_hexa ss, 4, 38, 30
 
-	imprimir_texto_mp msg_debug_eflags, 6, 0x70, 40, 27
-	;imprimir_hexa eflags, 8, 40, 30
+; 	pushfd
+; 	pop eax
+; 	imprimir_texto_mp msg_debug_eflags, 6, 0x70, 40, 27
+; 	imprimir_hexa eax, 8, 40, 30
 
-	imprimir_texto_mp msg_debug_cr0, 3, 0x70, 10, 40
-	;imprimir_hexa cr0, 4, 10, 44
+; 	mov eax, cr0
+; 	imprimir_texto_mp msg_debug_cr0, 3, 0x70, 10, 40
+; 	imprimir_hexa cr0, 4, 10, 44
 
-	imprimir_texto_mp msg_debug_cr2, 3, 0x70, 12, 40
-	;imprimir_hexa cr2, 4, 12, 44
+; 	mov eax, cr2
+; 	imprimir_texto_mp msg_debug_cr2, 3, 0x70, 12, 40
+; 	imprimir_hexa cr2, 4, 12, 44
 
-	imprimir_texto_mp msg_debug_cr3, 3, 0x70, 14, 40
-	;imprimir_hexa cr3, 4, 14, 44
+; 	mov eax, cr3
+; 	imprimir_texto_mp msg_debug_cr3, 3, 0x70, 14, 40
+; 	imprimir_hexa cr3, 4, 14, 44
 
-	imprimir_texto_mp msg_debug_cr4, 3, 0x70, 16, 40
-	;imprimir_hexa cr4, 4, 16, 44
+; 	mov eax, cr4
+; 	imprimir_texto_mp msg_debug_cr4, 3, 0x70, 16, 40
+; 	imprimir_hexa cr4, 4, 16, 44
 
-	imprimir_texto_mp msg_debug_stack, 5, 0x70, 27, 40
+; 	imprimir_texto_mp msg_debug_stack, 5, 0x70, 27, 40
 
-	;pushad
-	;pushfd
-	;; --- TERMINE DE IMPRIMIR LOS REGISTROS ---
+; 	;pushad
+; 	;pushfd
+; 	;; --- TERMINE DE IMPRIMIR LOS REGISTROS ---
 
-	ret
+; 	ret
